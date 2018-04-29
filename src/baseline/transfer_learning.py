305110151571d9d20daedcf018ca44f403ae687e
Joel Niklaus <jniklaus@fastforward.ch>
2018-04-29 10:18:42 +0200

    formatted baseline code

--- src/baseline/transfer_learning.py
+++ src/baseline/transfer_learning.py
@@ -4,6 +4,7 @@ Start with inceptionnet/resnet/vgg pretrained on imagenet
 add own classifier on top of it: flatten, dense, relu, dropout, dense, sigmoid
 train own classifier, freeze feature extractor net
 train own classifier and last convnet block
+(train with small learning rate and sgd to not destroy previously learned features)
 
 Be happy and hopefully win the competition ;)
 
