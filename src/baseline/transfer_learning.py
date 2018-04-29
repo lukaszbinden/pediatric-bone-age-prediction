0282ccda85d60952c6f280c20c15d11f3acf6974
Joel Niklaus <jniklaus@fastforward.ch>
2018-04-29 11:56:48 +0200

    added requirements.txt file

--- src/baseline/transfer_learning.py
+++ src/baseline/transfer_learning.py
@@ -1,6 +1,6 @@
 '''
 Idea:
-Start with inceptionnet/resnet/vgg pretrained on imagenet
+Start with inceptionnet/resnet/vgg pretrained on imagenet: https://keras.io/applications/
 add own classifier on top of it: flatten, dense, relu, dropout, dense, sigmoid
 train own classifier, freeze feature extractor net
 train own classifier and last convnet block
@@ -10,3 +10,12 @@ Be happy and hopefully win the competition ;)
 
 
 '''
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+
+conv_base_model = InceptionResNetV2(include_top=True,
+                                    weights='imagenet',
+                                    input_tensor=None,
+                                    input_shape=None,
+                                    pooling=None,
+                                    classes=1000)
+
