5226fe87e8dbdce6e5b90a6026f30e115d55d69a
Joel Niklaus <jniklaus@fastforward.ch>
2018-05-08 16:47:10 +0200

    adapted sh files to run RSNA16BitNet on cluster

---
+++ src/baseline/q_RSNA16BitNet.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -N RSNA16BitNet.py
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 1
+
+# Array job: -t <range>
+#$ -t 1
+
+#$ -l h=node03
+
+#$ -v DISPLAY
+
+#$ -o RSNA16BitNet.log
+
+#$ -m ea
+#$ -M joel.niklaus@students.unibe.ch, lukas.zbinden@unifr.ch
+
+python RSNA16BitNet.py
