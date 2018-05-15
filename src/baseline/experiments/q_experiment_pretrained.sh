2d118ad48605f1cf2312b1143e7b1c626824d00c
Joel Niklaus <jniklaus@fastforward.ch>
2018-05-15 17:01:26 +0200

    some refactoring added experiments

---
+++ src/baseline/experiments/q_experiment_pretrained.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#$ -N experiment_pretrained.py
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 1
+
+# Array job: -t <range>
+#$ -t 1
+
+#$ -l h=node03
+
+#$ -v DISPLAY
+
+#$ -o /var/tmp/studi5/boneage/git/jmcs-atml-bone-age-prediction/src/baseline/logs/experiment_pretrained.log
+
+#$ -m ea
+#$ -M joel.niklaus@students.unibe.ch,lukas.zbinden@unifr.ch
+
+python experiment_pretrained.py
