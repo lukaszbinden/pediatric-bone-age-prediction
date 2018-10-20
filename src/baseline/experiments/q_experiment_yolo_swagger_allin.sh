092d117f901b4b183fd1204913ed07b92d0bb348
lukaszbinden <lukas.zbinden@unifr.ch>
2018-10-20 15:56:03 +0200

    fix qsub script

--- src/baseline/experiments/q_experiment_yolo_swagger_allin.sh
+++ src/baseline/experiments/q_experiment_yolo_swagger_allin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.bash_profile
+
 #$ -N experiment_yolo_swagger_allin.py
 #$ -S /bin/bash
 #$ -cwd
@@ -9,11 +11,11 @@
 # Array job: -t <range>
 #$ -t 1
 
-#$ -l h=node03
+#$ -l h=node02
 
 #$ -v DISPLAY
 
-#$ -o /var/tmp/studi5/boneage/git/jmcs-atml-bone-age-prediction/src/baseline/logs/experiment_age_range.log
+#$ -o /data/cvg/lukas/logs/pediatric-bone-age-prediction/experiment_all-in.log
 
 #$ -m ea
 #$ -M joel.niklaus@students.unibe.ch,lukas.zbinden@unifr.ch
